4b506e865a6997fd24b39335a96cfeea7d14c209
dec05eba <dec05eba@protonmail.com>
2024-12-29 16:00:52 +0100

    Make systemctl work in flatpak

--- src/gui/GlobalSettingsPage.cpp
+++ src/gui/GlobalSettingsPage.cpp
@@ -77,7 +77,7 @@ namespace gsr {
 
             const char *args[] = { "systemctl", enable ? "enable" : "disable", "--user", "gpu-screen-recorder-ui", nullptr };
             std::string stdout_str;
-            const int exit_status = exec_program_get_stdout(args, stdout_str);
+            const int exit_status = exec_program_on_host_get_stdout(args, stdout_str);
             if(on_startup_changed)
                 on_startup_changed(enable, exit_status);
         };
@@ -109,7 +109,7 @@ namespace gsr {
 
         const char *args[] = { "systemctl", "is-enabled", "--quiet", "--user", "gpu-screen-recorder-ui", nullptr };
         std::string stdout_str;
-        const int exit_status = exec_program_get_stdout(args, stdout_str);
+        const int exit_status = exec_program_on_host_get_stdout(args, stdout_str);
         startup_radio_button_ptr->set_selected_item(exit_status == 0 ? "start_on_system_startup" : "dont_start_on_system_startup", false, false);
     }
 
