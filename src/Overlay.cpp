9aebe81ec4c248b96f0023d80a08b684a40508a0
dec05eba <dec05eba@protonmail.com>
2025-01-27 11:56:34 +0100

    amend

--- src/Overlay.cpp
+++ src/Overlay.cpp
@@ -774,11 +774,11 @@ namespace gsr {
 
         XcursorImage *cursor_image = nullptr;
         for(const char *cursor_theme_test : {cursor_theme, "default"}) {
-            for(unsigned int shape : {XC_left_ptr, XC_right_ptr}) {
-                cursor_image = XcursorShapeLoadImage(shape, cursor_theme_test, cursor_size);
-                if(cursor_image)
-                    break;
-            }
+            //for(unsigned int shape : {XC_left_ptr, XC_right_ptr}) {
+            cursor_image = XcursorShapeLoadImage(XC_left_ptr, cursor_theme_test, cursor_size);
+            if(cursor_image)
+                break;
+            //}
         }
 
         if(!cursor_image) {
