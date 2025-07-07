dd7aae31914c7efb659cc21f9e97f611d3aee7cb
dec05eba <dec05eba@protonmail.com>
2025-07-07 03:59:59 +0200

    Fix window capture not working in replay (thanks crosscoder)

--- src/Overlay.cpp
+++ src/Overlay.cpp
@@ -2484,7 +2484,7 @@ namespace gsr {
 
         const SupportedCaptureOptions capture_options = get_supported_capture_options(gsr_info);
         recording_capture_target = get_capture_target(config.replay_config.record_options.record_area_option, capture_options);
-        if(!validate_capture_target(recording_capture_target, capture_options)) {
+        if(!validate_capture_target(config.replay_config.record_options.record_area_option, capture_options)) {
             char err_msg[256];
             snprintf(err_msg, sizeof(err_msg), "Failed to start replay, capture target \"%s\" is invalid.\nPlease change capture target in settings", recording_capture_target.c_str());
             show_notification(err_msg, notification_error_timeout_seconds, mgl::Color(255, 0, 0), mgl::Color(255, 0, 0), NotificationType::REPLAY);
