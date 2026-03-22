c3e9aa0f81e7bc109cb7e3b69a2bc52da55c0c2c
dec05eba <dec05eba@protonmail.com>
2026-03-22 03:04:27 +0100

    Add french language option in settings

--- src/gui/GlobalSettingsPage.cpp
+++ src/gui/GlobalSettingsPage.cpp
@@ -529,6 +529,7 @@ namespace gsr {
         combo_box->add_item(TR("System language"), "");
         combo_box->add_item("English", "en");
         combo_box->add_item("Español", "es");
+        combo_box->add_item("Français", "fr");
         combo_box->add_item("Русский", "ru");
         combo_box->add_item("Українська", "uk");
         combo_box->on_selection_changed = [](const std::string&, const std::string &id) {
